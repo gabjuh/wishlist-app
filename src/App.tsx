f98ce083adaa5414cee7d5ca2e5a134f4687d02d
Gábor Juhász <g.juhasz@neusta.de>
2024-08-06 13:02:51 +0200

    add routing with home and impressum

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,56 @@
-import './App.css'
-import ProductTable from './components/ProductTable';
-import AddProduct from './components/AddProduct';
-import Nav from './components/Nav';
-import { useState } from 'react';
-import Letter from './components/Letter';
-import Footer from './components/Footer';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Impressum from "./pages/Impressum";
 
 function App() {
-
-  const [isProductFormActive, setIsProductFormActive] = useState<boolean>(false);
+  const [isProductFormActive, setIsProductFormActive] =
+    useState<boolean>(false);
   const [selectedId, setSelectedId] = useState<string | undefined>();
-  const isAdminLoggedIn = true;
+  const isAdminLoggedIn = false;
 
   const onClose = () => {
     setIsProductFormActive(false);
     setSelectedId(undefined);
-  }
+  };
 
   const onEditHandler = (id: string) => {
     setIsProductFormActive(true);
     setSelectedId(id);
-  }
+  };
 
   return (
-    <>
-      <Nav 
-        isAdminLoggedIn={isAdminLoggedIn} 
-        setIsProductFormActive={setIsProductFormActive}
-      />
-      {isAdminLoggedIn && isProductFormActive && 
-        <AddProduct 
-          onClose={onClose} 
-          selectedId={selectedId} 
-        />}
-      
-      <div className="text-[#ddd] bg-gradient-to-b from-[#606888] to-[#3f4a6b] py-10">
-        <Letter />
-      </div>
-      <ProductTable
-        onEditHandler={onEditHandler}
-        isAdminLoggedIn={isAdminLoggedIn}
-      />
-      <Footer />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              isAdminLoggedIn={isAdminLoggedIn}
+              onEditHandler={onEditHandler}
+              isProductFormActive={isProductFormActive}
+              setIsProductFormActive={setIsProductFormActive}
+              onClose={onClose}
+              selectedId={selectedId}
+            />
+          }
+        />
+        <Route
+          path="/impressum"
+          element={
+            <Impressum
+              isAdminLoggedIn={isAdminLoggedIn}
+              isProductFormActive={isProductFormActive}
+              setIsProductFormActive={setIsProductFormActive}
+              onClose={onClose}
+              selectedId={selectedId}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
